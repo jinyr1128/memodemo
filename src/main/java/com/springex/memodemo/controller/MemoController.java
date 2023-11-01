dfc92a411c0fb96de6da0d366b8085c497ee9ca3
jinyr1128 <jinyr1128@gmail.com>
2023-11-01 17:30:08 +0900

    컨테이너 등록

--- src/main/java/com/springex/memodemo/controller/MemoController.java
+++ src/main/java/com/springex/memodemo/controller/MemoController.java
@@ -15,36 +15,27 @@ public class MemoController {
     private final MemoService memoService;
 
     public MemoController(MemoService memoService) {
-
         this.memoService = memoService;
     }
 
     @PostMapping("/memos")
     public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto) {
-
-
         return memoService.createMemo(requestDto);
     }
 
     @GetMapping("/memos")
     public List<MemoResponseDto> getMemos() {
-
         return memoService.getMemos();
-
-
     }
 
     @PutMapping("/memos/{id}")
     public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
-
-        return memoService.updateMemo(id,requestDto);
+        return memoService.updateMemo(id, requestDto);
     }
 
     @DeleteMapping("/memos/{id}")
     public Long deleteMemo(@PathVariable Long id) {
-
         return memoService.deleteMemo(id);
-
     }
 
 }
--- src/main/java/com/springex/memodemo/service/MemoService.java
+++ src/main/java/com/springex/memodemo/service/MemoService.java
@@ -4,9 +4,11 @@ import com.springex.memodemo.dto.MemoRequestDto;
 import com.springex.memodemo.dto.MemoResponseDto;
 import com.springex.memodemo.entity.Memo;
 import com.springex.memodemo.repository.MemoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
-
+@Component
 public class MemoService {
     private final MemoRepository memoRepository;
 
@@ -31,19 +33,17 @@ public class MemoService {
     public List<MemoResponseDto> getMemos() {
 
 
-        return  memoRepository.findAll();
+        return memoRepository.findAll();
     }
 
     public Long updateMemo(Long id, MemoRequestDto requestDto) {
 
 
-
-
         // 해당 메모가 DB에 존재하는지 확인
-        Memo memo =memoRepository.findById(id);
-        if(memo != null) {
+        Memo memo = memoRepository.findById(id);
+        if (memo != null) {
             // memo 내용 수정
-            memoRepository.update(id ,requestDto);
+            memoRepository.update(id, requestDto);
             return id;
         } else {
             throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
@@ -55,7 +55,7 @@ public class MemoService {
 
         // 해당 메모가 DB에 존재하는지 확인
         Memo memo = memoRepository.findById(id);
-        if(memo != null) {
+        if (memo != null) {
             // memo 삭제
             memoRepository.delete(id);
             return id;
