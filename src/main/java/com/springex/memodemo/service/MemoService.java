bb3ce4c2d18a9c78eed9c10a7247edcc936965a1
jinyr1128 <jinyr1128@gmail.com>
2023-11-06 16:55:51 +0900

    Spring Dara JPA?

--- src/main/java/com/springex/memodemo/service/MemoService.java
+++ src/main/java/com/springex/memodemo/service/MemoService.java
@@ -4,8 +4,10 @@ import com.springex.memodemo.dto.MemoRequestDto;
 import com.springex.memodemo.dto.MemoResponseDto;
 import com.springex.memodemo.entity.Memo;
 import com.springex.memodemo.repository.MemoRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+
 public class MemoService {
     private final MemoRepository memoRepository;
 
@@ -30,34 +32,32 @@ public class MemoService {
     public List<MemoResponseDto> getMemos() {
 
 
-        return memoRepository.findAll();
+        return memoRepository.findAll().stream().map(MemoResponseDto::new).toList();
     }
 
+    @Transactional
     public Long updateMemo(Long id, MemoRequestDto requestDto) {
-
-
         // 해당 메모가 DB에 존재하는지 확인
-        Memo memo = memoRepository.findById(id);
-        if (memo != null) {
-            // memo 내용 수정
-            memoRepository.update(id, requestDto);
-            return id;
-        } else {
-            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
-        }
+        Memo memo = findMemo(id);
+        // memo 내용 수정
+        memo.update(requestDto);
+        return id;
     }
-
 
     public Long deleteMemo(Long id) {
 
         // 해당 메모가 DB에 존재하는지 확인
-        Memo memo = memoRepository.findById(id);
-        if (memo != null) {
-            // memo 삭제
-            memoRepository.delete(id);
-            return id;
-        } else {
-            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
-        }
+        Memo memo = findMemo(id);
+
+
+        // memo 삭제
+        memoRepository.delete(memo);
+        return id;
+    }
+
+    private Memo findMemo(Long id) {
+        return memoRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("선택한 메모는 존재하지 않습니다")
+        );
     }
 }
