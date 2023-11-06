78438505986a02eb6683cada1d05e5676ac7b92b
jinyr1128 <jinyr1128@gmail.com>
2023-11-06 17:25:11 +0900

    Query

--- src/main/java/com/springex/memodemo/repository/MemoRepository.java
+++ src/main/java/com/springex/memodemo/repository/MemoRepository.java
@@ -4,6 +4,11 @@ import com.springex.memodemo.entity.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public interface MemoRepository extends JpaRepository<Memo,Long> {
+    List<Memo> findAllByOrderByModifiedAtDesc();
+
+
 }
